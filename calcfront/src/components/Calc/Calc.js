9fc5f2d2414e2273ddf6c346ac7bff61bafb9e92
Ledwos <32643853+Ledwos@users.noreply.github.com>
2020-10-13 11:58:14 +0100

    btn clicks show on display

--- calcfront/src/components/Calc/Calc.js
+++ calcfront/src/components/Calc/Calc.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Keypad from '../Keypad/Keypad';
 
 const Calc = () => {
+    const [cFace, setcFace] = useState('');
+
+    const handleClick = (e) => {
+        let btnText = e.target.innerHTML;
+        let nuValue = cFace + btnText;
+        setcFace(nuValue);
+        // console.log(btnText);
+    }
     return (
         <div>
-            <p>I'm the calc</p>
-            <Keypad />
+            <div id='cFace'>
+                {cFace}
+            </div>
+            <Keypad handleClick={handleClick} />
         </div>
     );
 };
--- calcfront/src/components/Keypad/Keypad.js
+++ calcfront/src/components/Keypad/Keypad.js
@@ -1,67 +1,67 @@
 import React from 'react';
 import './Keypad.css';
 
-const Keypad = () => {
+const Keypad = (props) => {
     return (
         <div id='KeypadDiv'>
             <div className='KeypadBtn'>
                 
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 CE
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 C
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 +
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 7
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 8
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 9
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 -
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 4
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 5
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 6
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 x
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 1
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 2
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 3
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 /
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 .
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 0
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 
             </div>
-            <div className='KeypadBtn'>
+            <div className='KeypadBtn' onClick={props.handleClick}>
                 =
             </div>
         </div>
